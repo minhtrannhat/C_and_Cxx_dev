04b993e856c72157bda0b8b6be3b88267c11a094
minhradz <minhradz@gmail.com>
2020-01-17 10:06:30 -0500

    Slight modification for better readability

--- intergral.cpp
+++ intergral.cpp
@@ -6,7 +6,7 @@ int main() {
     double d = 6.15 - 3.15;
     
     std::cout << std::setprecision(17);
-    std::cout << d << std::endl;
+    std::cout << "The value of d is actually is " <<  d << std::endl;
 
     std::cout << std::boolalpha;
     std::cout << (d == 3.00);
